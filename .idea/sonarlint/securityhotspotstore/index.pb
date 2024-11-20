86bd0c8899535d4bcae9a66b1afb40512730733e
kivabarnes <kivabarnes@gmail.com>
2024-11-20 12:54:28 +0100

    Add test cases for DOA

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 G
 contracts/dao-core.clar,b/c/bcda9195d0fccfbb4b41d5d99240281c812cceb6
+F
+tests/dao-core.test.ts,d/8/d815a26030ed4467798867fb340919efaa24dac0
--- tests/dao-core.test.ts
+++ tests/dao-core.test.ts
@@ -1,21 +1,27 @@
+;; tests/dao-core_test.ts
 
-import { describe, expect, it } from "vitest";
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v0.14.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
-const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
-
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
-
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+Clarinet.test({
+  name: "Ensure that users can join and leave the DAO",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const wallet1 = accounts.get('wallet_1')!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall('dao-core', 'join-dao', [], wallet1.address),
+    ]);
+    assertEquals(block.receipts[0].result, '(ok true)');
+    
+    let result = chain.callReadOnlyFn('dao-core', 'is-member', [types.principal(wallet1.address)], wallet1.address);
+    assertEquals(result.result, 'true');
+    
+    block = chain.mineBlock([
+      Tx.contractCall('dao-core', 'leave-dao', [], wallet1.address),
+    ]);
+    assertEquals(block.receipts[0].result, '(ok true)');
+    
+    result = chain.callReadOnlyFn('dao-core', 'is-member', [types.principal(wallet1.address)], wallet1.address);
+    assertEquals(result.result, 'false');
+  },
 });
